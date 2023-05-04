cea0d379170efbc1ba728c7f4f167496bfc52976
nullsec45 <rama21010ti@student.nurulfikri.ac.id>
2023-05-05 00:15:44 +0700

    laravel resto api

---
+++ routes/api.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthController;
+use App\Http\Controllers\{RegisterController,CategoryController};
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| is assigned the "api" middleware group. Enjoy building your API!
+|
+*/
+
+// Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+//     return $request->user();
+// });
+
+Route::group(["middleware" => "api"], function($router){
+    Route::prefix("auth")->group(function(){
+        Route::post("register",[RegisterController::class,"register"])->name("register");
+        Route::post("login",[AuthController::class,"login"])->name("login");
+        Route::post("logout",[AuthController::class,"logout"])->name("logout");
+        Route::post("refresh",[AuthController::class,"refresh"])->name("refresh");
+        Route::post("me",[AuthController::class,"me"])->name("me");
+    });
+
+    Route::prefix("products")->group(function(){
+        Route::resource('category', CategoryController::class);
+    });
+});

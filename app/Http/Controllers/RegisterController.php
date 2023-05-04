0f85a5a7f9fd4decef324a0959d0c687934d27cf
nullsec45 <rama21010ti@student.nurulfikri.ac.id>
2023-05-05 00:08:43 +0700

    register controller

---
+++ app/Http/Controllers/RegisterController.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Validator;
+ 
+class RegisterController extends Controller
+{
+    public function register(){
+        $validator=validator::make(request()->all(),[
+            "username" => "required|unique:users",
+            "email" => "required|email|unique:users",
+            "password" => "required",
+            "role_id" => "required|numeric"
+        ]);
+
+        if($validator->fails()){
+            return response()->json($validator->messages());
+        }
+
+        $user=User::create([
+            "username" => request("username"),
+            "email" => request("email"),
+            "password" => Hash::make(request("password")),
+            "role_id" => request("role_id")
+        ]);
+        
+        return response()->json(["message" => "Successfully register!"]);
+    }
+
+}

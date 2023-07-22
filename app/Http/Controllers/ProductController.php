898c64ec6c6713133f5251efa0f42f06d5c97595
nullsec45 <rama21010ti@student.nurulfikri.ac.id>
2023-07-22 23:43:25 +0700

    method index, show, update, validateRequest

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -4,8 +4,10 @@ namespace App\Http\Controllers;
 
 use App\Models\Product;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 use App\Http\Controllers\{AuthUserTrait};
 use Illuminate\Support\Facades\Validator;
+use Illuminate\Support\Facades\Storage;
 
 
 class ProductController extends Controller
@@ -19,7 +21,10 @@ class ProductController extends Controller
 
     public function index()
     {
-        //
+        $data=DB::table("products")
+            ->leftJoin("categories","products.category_id","=","categories.id")
+            ->select("products.id","kode","nama","stok","gambar","categories.category as category")->get();
+        return response()->json($data);
     }
 
     /**
@@ -45,6 +50,7 @@ class ProductController extends Controller
                         "nama" => $request->nama,
                         "stok" => $request->stok,
                         "gambar" => $imageName,
+                        "price"  => $request->price,
                         "category_id" => $request->category_id
                     ]
         );
@@ -58,23 +64,47 @@ class ProductController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function show($id)
+    public function show($product)
     {
-        //
         auth()->shouldUse("api");
         $this->getAuthUser();  
+
+        $data=DB::table("products")->where("id", $product)->get();
+
+        if(count($data) == 0){
+            return response()->json(["message" => "Product Not Found"]);
+        }
+        return response()->json($data);
     }
 
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, $id)
+  
+    public function update(Request $request, $product)
     {
-        //
+        auth()->shouldUse("api");
+        $this->getAuthUser();  
+
+        $image=$request->file("gambar");
+        $uploadFolder="products";
+
+        // $this->validateRequest($request,"update");
+        $product = Product::find($product);
+        $product->nama=$request->nama;
+        $product->stok=$request->stok;
+        if($image){
+            $imageName="product_".strtolower($product->kode)."_".$image->hashName();
+            $image->storeAs($uploadFolder, $imageName, 'public');
+            if (file_exists("app/public/storage/products".$product->gambar)) {
+                // @unlink($image_path);         
+                @unlink("app/public/storage/products".$product->gambar);
+            }
+            $product->gambar=$imageName;
+        }
+        $product->price=$request->price;
+        $product->category_id=$request->category_id;
+        $product->save();
+
+        return response()->json(["message" => "Successfully Updated Product"]);
+        
     }
 
     /**
@@ -90,14 +120,26 @@ class ProductController extends Controller
         $this->getAuthUser();  
     }
 
-    protected function validateRequest($request){
-        $validator=Validator::make($request->all(),[
-            "kode" => "required|unique:products",
+    protected function validateRequest($request, $type="insert"){
+        $rules=[
             "nama" => "required|unique:products",
+            "kode" => "required|unique:products",
             "stok" => "required|numeric",
             "gambar" => "required|image:jpeg,png,jpg,gif,svg|max:2048",
+            "price" => "required|numeric",
             "category_id" => "required|numeric"
-        ]);
+        ];
+
+        if($type == "update"){
+            $rules["nama"]="unique:products";
+            $rules["kode"]="unique:products";
+            $rules["stok"]="numeric";
+            $rules["gambar"]="image:jpeg,png,jpg,gif,svg|max:2048";
+            $rules["price"]="numeric";
+            $rules["category_id"]="numeric";
+        }
+
+        $validator=Validator::make($request->all(), $rules);
 
         if($validator->fails()){
             response()->json($validator->messages(),422)->send();

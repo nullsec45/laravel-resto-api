75f6d963400cf505f91c9de8585a22be44ab3b4d
nullsec45 <rama21010ti@student.nurulfikri.ac.id>
2023-08-17 12:37:09 +0700

    RoleFitur:table, guarded, timestamps, role(), fitur()

--- app/Models/RoleFitur.php
+++ app/Models/RoleFitur.php
@@ -8,4 +8,17 @@ use Illuminate\Database\Eloquent\Model;
 class RoleFitur extends Model
 {
     use HasFactory;
+
+    protected $table="roles_fitur",
+              $guarded=["id"];
+
+    public $timestamps=false;
+
+    public function role():BelongsTo{
+        return $this->belongsTo(Role::class);
+    }
+
+    public function fitur():BelongsTo{
+        return $this->belongsTo(Fitur::class);
+    }
 }

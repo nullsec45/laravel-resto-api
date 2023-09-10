98c298ad12a99e98e55d6879dd4321c17be6eea5
nullsec45 <rama21010ti@student.nurulfikri.ac.id>
2023-09-10 11:19:33 +0700

    n:dafrtar_meja_controller

---
+++ app/Http/Controllers/DaftarMejaController.php
@@ -0,0 +1,153 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\DaftarMeja;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+use App\Http\Controllers\{AuthUserTrait};
+use Illuminate\Support\Facades\Validator;
+
+
+
+class DaftarMejaController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    use AuthUserTrait;
+
+    public function index()
+    {
+        auth()->shouldUse("api");
+        $this->getAuthUser();
+        
+        $data=DB::table("daftar_meja")->select()->get();
+
+        return response()->json($data);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        auth()->shouldUse("api");
+        $this->getAuthUser();  
+
+        $this->validateRequest($request);
+
+        DaftarMeja::create(
+                              ["kode" => $request->kode, 
+                               "kapasitas" => $request->kapasitas,
+                              ]);
+
+        return response()->json(["message" => "Successfully Created Daftar Meja","status" => 200], 200);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($kode)
+    {
+        auth()->shouldUse("api");
+        $this->getAuthUser();  
+
+        $data=DaftarMeja::select()->where("kode", $kode)->first();
+
+        if(!$data){
+            return resposne()->json(["message" => "Daftar Meja Not Found","status" => 422], 422);
+        }
+        return response()->json($data);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $kode)
+    {
+        auth()->shouldUse("api");
+        $this->getAuthUser();
+
+
+        $DaftarMeja=DaftarMeja::find($kode);
+
+        if(!$DaftarMeja){
+            return response()->json(["message" => "Daftar Meja Not Found", 422], 422);
+        }
+        $kode=$request->kode;
+
+        $this->validateRequest($request);
+
+        $DaftarMeja->update([
+            "kode" => $kode,
+            "kapasitas" => $request->kapasitas
+        ]);
+
+        $DaftarPemesan=$DaftarMeja->daftar_pemesan()->first();
+        if($DaftarPemesan){
+            $DaftarPemesan->kode_meja=$kode;
+            $DaftarPemesan->save();
+        }
+
+        return response()->json(["message" => "Successfully Updated Daftar Meja","status" => 200], 200);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($kode)
+    {
+        auth()->shouldUse("api");
+        $this->getAuthUser();
+
+        $DaftarMeja=DaftarMeja::find($kode);
+
+        if(!$DaftarMeja){
+            return response()->json(["message" => "Daftar Meja Not Found", 422], 422);
+        }
+
+        $DaftarPemesan=$DaftarMeja->daftar_pemesan()->get();
+        if($DaftarPemesan){
+            foreach($DaftarMeja->daftar_pemesan()->get() as $pemesan) {
+                $pemesan->update(["kode_meja" => NULL]);
+            }
+        }
+
+        $DaftarMeja->delete();
+
+        return response()->json(["message" => "Successfully Deleted Daftar Meja","status" => 200], 200);
+    }
+
+    private function validateRequest($request, $type="insert"){
+        $rules=[
+            "kode" => "required|unique:daftar_meja",
+            "kapasitas" => "required|numeric",
+        ];
+
+
+        $validator=Validator::make($request->all(), $rules);
+
+        if($validator->fails()){
+            response()->json($validator->messages(),422)->send();
+            exit;
+        }
+    }
+
+    
+}

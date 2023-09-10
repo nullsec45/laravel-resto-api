0133109008a33d62e60c2523d29e165aa2acfc1a
nullsec45 <rama21010ti@student.nurulfikri.ac.id>
2023-09-10 11:27:21 +0700

    n:migratio daftar meja table

---
+++ database/migrations/2023_09_09_100109_create_daftar_meja_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('daftar_meja', function (Blueprint $table) {
+            $table->string("kode", 10)->primary();
+            $table->integer("kapasitas");
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('daftar_meja');
+    }
+};

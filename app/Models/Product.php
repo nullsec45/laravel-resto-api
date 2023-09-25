e1a02157dfa412a52ba9715cac0d92ffdfc7b805
nullsec45 <rama21010ti@student.nurulfikri.ac.id>
2023-09-26 00:16:20 +0700

    Product:pesanan relational

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -16,4 +16,8 @@ class Product extends Model
     public function category(){
             return $this->belongsTo(Category::class);
     }
+
+    public function pesanan(){
+        return $this->belongsTo(Pesanan::class);
+    }
 }

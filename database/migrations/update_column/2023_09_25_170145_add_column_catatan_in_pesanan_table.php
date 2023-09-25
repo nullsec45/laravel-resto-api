5b1c50e3a280d1d7830f2b8ce17918cafa0305e9
nullsec45 <rama21010ti@student.nurulfikri.ac.id>
2023-09-26 00:17:47 +0700

    daftar_pesanan_migration:add column catatan & remove catatan

---
+++ database/migrations/update_column/2023_09_25_170145_add_column_catatan_in_pesanan_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('pesanan', function (Blueprint $table) {
+            $table->tinyText("catatan")->after("product_id")->default("-");
+        });
+
+        Schema::table('daftar_pemesan', function (Blueprint $table) {
+            $table->dropColumn("catatan");
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('pesanan', function (Blueprint $table) {
+            $table->dropColumn("catatan");
+        });
+    }
+};

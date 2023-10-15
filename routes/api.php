76021b2c9443b77ecc2548801057dcbbc1bf64ba
nullsec45 <rama21010ti@student.nurulfikri.ac.id>
2023-10-15 12:57:14 +0700

    implements rate limier in routes

--- routes/api.php
+++ routes/api.php
@@ -32,7 +32,7 @@ use App\Http\Controllers\{RegisterController,
 Route::group(["middleware" => "api"], function($router){
     Route::prefix("auth")->group(function(){
         Route::post("register",[RegisterController::class,"register"])->name("register");
-        Route::post("login",[AuthController::class,"login"])->name("login");
+        Route::post("login",[AuthController::class,"login"])->name("login")->middleware("throttle:login");
         Route::post("logout",[AuthController::class,"logout"])->name("logout");
         Route::post("refresh",[AuthController::class,"refresh"])->name("refresh");
         Route::post("me",[AuthController::class,"me"])->name("me");
